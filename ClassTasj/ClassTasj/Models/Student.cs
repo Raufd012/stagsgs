0e99c78be7f538ddcee2d033438e24982866897e
nazeani <tu7mp4w99@code.edu.az>
2024-04-08 09:41:34 +0400

    initial commit

---
+++ ClassTasj/ClassTasj/Models/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTasj.Models
+{
+    internal class Student
+    {
+        private static int Count = 0;
+        public int Id { get; }
+        public string Fullname { get; }
+
+        public Student(string fullname)
+        {
+            if (!CheckFullname(fullname))
+                throw new ArgumentException("Invalid fullname format. Fullname must consist of a first and last name.");
+
+            Fullname = fullname;
+            Count++;
+            Id = Count;
+        }
+
+        public static bool CheckFullname(string fullname)
+        {
+            if (string.IsNullOrWhiteSpace(fullname))
+                return false;
+
+            string[] parts = fullname.Split(' ');
+            if (parts.Length != 2)
+                return false;
+
+            foreach (string part in parts)
+            {
+                if (string.IsNullOrWhiteSpace(part) || !char.IsLetter(part[0]))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}. {Fullname}";
+        }
+    }
+}
---
+++ ClassTasj/ClassTasj/Program.cs
@@ -0,0 +1,24 @@
+﻿using ClassTasj.Models;
+
+namespace ClassTasj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                Student student1 = new Student("Dostuyev Rauf");
+                Console.WriteLine(student1);
+
+                Student student2 = new Student("Ulvi Abdullazada");
+                Console.WriteLine(student2);
+
+                Student student3 = new Student("Sabir Guliyev");
+                Console.WriteLine(student3);
+            }
+
+        }
+    }
+    
+}
